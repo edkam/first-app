646236918338731cd0a8c61018e2abc942fe5805
ed <edz.eky@hotmail.com>
2017-03-30 13:58:27 +1100

    my first commit

--- first app/ViewController.swift
+++ first app/ViewController.swift
@@ -10,10 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coolLabel: UILabel!
+var tapCount = 0
+    @IBAction func button2Pressed(_ sender: Any) {
+        coolLabel.text = "Dont press this button"
+    }
+    @IBAction func buttonPressed(_ sender: Any) {
+        tapCount = tapCount + 1
+        if tapCount >= 5 {
+            coolLabel.text = "I told you not to tap that shit"
+        
+        }else{
+            coolLabel.text = "DROOOOP the BASSSSS!!"
+        }
+
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+       }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
